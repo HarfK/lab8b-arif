c92ef1b76e3d54a4221b3095ebd7f0e0336aa520
Arif <arifhaikal040818@gmail.com>
2026-06-18 23:09:28 +0800

    commit

--- app/src/main/java/com/example/lab8b_arif/MainActivity.java
+++ app/src/main/java/com/example/lab8b_arif/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.lab8b_arif;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button openMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        openMap = (Button) findViewById(R.id.button);
+    }
+    public void openMap(View view){
+        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+        startActivity(intent);
     }
 }
